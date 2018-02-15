0dbe4f3d23b1a93eb0e876e1a446a67bf67ff7d9
Rebecca Lynde-Scott <MsMadGirl@users.noreply.github.com>
2018-02-14 18:12:52 -0800

    Update Geomantic.rb

--- Geomantic.rb
+++ Geomantic.rb
@@ -1,7 +1,55 @@
-require yaml
-require pg
+#!/usr/bin/ruby
 
-#something to take in info about a chart and pass it to the DB
+require 'yaml'
+require 'sqlite3'
+
+#something to create database
+
+SQLite3::Database.new "geomantic"
+
+#exception handling for db
+
+begin
+	db = SQLite3::Database.open "geomantic"
+    puts db.get_first_value 'SELECT SQLITE_VERSION()'
+    db.execute "CREATE TABLE IF NOT EXISTS 
+    	Chart(Id INTEGER PRIMARY KEY, 
+    	Chart_name TEXT, Date TEXT, For TEXT, 
+    	BY TEXT, Subject TEXT, M1_fire INTEGER, 
+    	M1_air INTEGER, M1_water INTEGER, 
+    	M1_earth INTEGER, M2_fire INTEGER,
+    	M2_air INTEGER, M2_water INTEGER,
+    	M2_earth INTEGER, M3_fire INTEGER,
+    	M3_air INTEGER, M3_water INTEGER,
+    	M3_earth INTEGER, M4_fire INTEGER,
+    	M4_air INTEGER, M4_water INTEGER,
+    	M4_earth INTEGER)"
+rescue SQLite3::Exception => e     
+    puts "Exception occurred"
+    puts e    
+ensure
+    db.close if db    
+end
+
+#something to take in info about a chart and 
+#pass it to the DB
+
+def chart_info
+	puts "Name of chart: "
+	chart_name = gets
+	puts "Date: "
+	date = gets
+	puts = "Who is the chart for? "
+	chart_for = gets
+	puts = "Who is the chart being created by? "
+	chart_by = gets
+	puts = "What is the question about? "
+	subject = gets
+
+	begin
+		db = SQLite3::Database.open "geomantic"
+		db.execute ""
+
 
 #generates a line
 
@@ -11,14 +59,15 @@ def generate_line
   	else 
     	line_value = 1
   	end
-  	return line_value
+
+  	puts line_value
 end
 
-#something to run generate_line four times to make a mother
+#run generate_line four times to make a mother
 
 def generate_mother
-	fire_line = fire_line.generate_line
-	air_line = air_line.generate_line
+	fire_line  = fire_line.generate_line
+	air_line   = air_line.generate_line
 	water_line = water_line.generate_line
 	earth_line = earth_line.generate_line
 
@@ -31,7 +80,7 @@ def generate_mother
 
 end
 
-4.generate_mother
+generate_mother
 
 #something to pass the mothers into the DB
 
